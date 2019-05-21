07b0da3282bfb10032e94fd1c74e0ba75e99b66a
Ken Riley <kriley314@gmail.com>
2019-05-21 14:28:53 -0500

    Initial insertion

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require( "express" );
+
+const mongoose = require( "mongoose" );
+const routes = require( "./routes" );
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use( express.urlencoded({ extended: true }));
+app.use( express.json());
+
+// Serve up static assets (usually on heroku)
+if ( process.env.NODE_ENV === "production" ) {
+  app.use( express.static( "client/build" ));
+}
+
+// Define API routes here
+app.use( routes );
+
+// Connect to the Mongo DB
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://user1:password1@ds125871.mlab.com:25871/heroku_0xn0jnk7",
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    }
+);
+
+// Start the API server
+app.listen( PORT, () => {
+  console.log( `🌎 ==> API server now on port ${PORT}!` );
+});
